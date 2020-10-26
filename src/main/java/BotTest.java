6bd3607a235e868511bcfcd2a454b9eafe96e62d
Raymond Wang <rywang@email.wm.edu>
2020-10-25 23:55:14 -0400

    Added a basic bot test along with some libraries

---
+++ src/main/java/BotTest.java
@@ -0,0 +1,24 @@
+import discord4j.core.DiscordClient;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.MessageChannel;
+
+public class BotTest {
+    public static void main(String[] args){
+        final String token = args[0];
+        final DiscordClient client = DiscordClient.create(token);
+        final GatewayDiscordClient gateway = client.login().block();
+
+        gateway.on(MessageCreateEvent.class).subscribe(event -> {
+            final Message message = event.getMessage();
+            if ("!ping".equals(message.getContent())) {
+                final MessageChannel channel = message.getChannel().block();
+                channel.createMessage("Pong!").block();
+            }
+        });
+
+        gateway.onDisconnect().block();
+    }
+}
