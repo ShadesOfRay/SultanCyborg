51aab7e50778542761473f2478879174bf9c4baf
William Chase <wchase99@gmail.com>
2020-10-26 19:53:23 -0400

    Pushing new json file

--- src/main/java/commands/DateCommand.java
+++ src/main/java/commands/DateCommand.java
@@ -1,18 +1,21 @@
 package commands;
+import java.io.FileReader;
 import java.util.ArrayList;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.channel.MessageChannel;
+import org.json.simple.*;
+import org.json.simple.parser.*;
 
 public class DateCommand implements Command{
     ArrayList<String> nameOfImportance = new ArrayList<String>();
 
     public String invoker()
     {
-        return "!date " + nameOfImportance;
+        return "date";
     }
     @Override
     public String info() {
-        return null;
+        return "Give the info about this date";
     }
 
     @Override
@@ -21,8 +24,27 @@ public class DateCommand implements Command{
     }
     public void action(MessageCreateEvent event, String[] arguments)
     {
-        final MessageChannel channel = event.getMessage().getChannel().block();
-        channel.createMessage("Pong!").block();
+        JSONParser readingRainbow = new JSONParser();
+
+        try
+        {
+            JSONObject jSonA = (JSONObject) readingRainbow.parse(new FileReader(("DatesAndNames.json")));
+
+            if(jSonA.containsValue(arguments[0]))
+            {
+                final MessageChannel channel = event.getMessage().getChannel().block();
+                channel.createMessage((String) jSonA.get(arguments[0])).block();
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+        catch(Exception e)
+        {
+            final MessageChannel channel = event.getMessage().getChannel().block();
+            channel.createMessage("WHAT A LOSER!").block();
+        }
     }
 
     public void log(MessageCreateEvent event){
