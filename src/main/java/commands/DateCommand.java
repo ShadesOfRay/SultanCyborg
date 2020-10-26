9597e3f60fe8ddfbe04f10f4a94977870fc881b4
William Chase <wchase99@gmail.com>
2020-10-26 00:53:13 -0400

    Started with the DateCommand but realized I needed to get a JSON file to store the date first.

--- src/main/java/commands/DateCommand.java
+++ src/main/java/commands/DateCommand.java
@@ -1,7 +1,23 @@
-package Commands;
-import java.time.*;
+package commands;
+import java.util.ArrayList;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.channel.MessageChannel;
 
-public class DateCommand
-{
-     
+public class DateCommand implements Command{
+    ArrayList<String> nameOfImportance = new ArrayList<String>();
+
+    public String invoker()
+    {
+        return "!date " + nameOfImportance;
+    }
+
+    public void action(MessageCreateEvent event)
+    {
+        final MessageChannel channel = event.getMessage().getChannel().block();
+        channel.createMessage("Pong!").block();
+    }
+
+    public void log(MessageCreateEvent event){
+        System.out.println("Ping command was used");
+    }
 }
