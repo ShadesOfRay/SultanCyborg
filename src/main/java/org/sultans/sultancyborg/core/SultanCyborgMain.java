c2762cc765c1ae9f34c3ffb4ab80815308c7fd62
Raymond Wang <rywang@email.wm.edu>
2020-11-26 22:38:38 -0500

    Fixed one word, optimized the imports

--- src/main/java/org/sultans/sultancyborg/core/SultanCyborgMain.java
+++ src/main/java/org/sultans/sultancyborg/core/SultanCyborgMain.java
@@ -1,23 +1,16 @@
 package org.sultans.sultancyborg.core;
 
 import discord4j.common.util.Snowflake;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.lifecycle.ReadyEvent;
-import discord4j.core.object.entity.Message;
+import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.User;
-import discord4j.core.object.entity.channel.Channel;
-import discord4j.core.object.entity.channel.GuildChannel;
 import discord4j.core.object.entity.channel.MessageChannel;
-import discord4j.core.object.entity.channel.TextChannel;
-import discord4j.discordjson.json.ImmutableMessageData;
-import discord4j.discordjson.json.gateway.ChannelCreate;
-import discord4j.discordjson.json.gateway.ImmutableMessageCreate;
 import org.sultans.sultancyborg.commands.Command;
 import org.sultans.sultancyborg.commands.DateAddCommand;
 import org.sultans.sultancyborg.commands.MangaCommand;
 import org.sultans.sultancyborg.commands.PingCommand;
-import discord4j.core.DiscordClientBuilder;
-import discord4j.core.GatewayDiscordClient;
-import discord4j.core.event.domain.message.MessageCreateEvent;
 
 import java.util.ArrayList;
 
@@ -42,11 +35,8 @@ public class SultanCyborgMain {
                     client.getChannelById(Snowflake.of("445071996360065054"))
                             .cast(MessageChannel.class)
                             .flatMap(messageChannel -> messageChannel.createMessage("Bot Online"))
-                            .then();
+                            .subscribe();
                 });
-
-
-
         //The message listener, which filters out messages from bots and then sends them to the parser
         client.getEventDispatcher().on(MessageCreateEvent.class)
                 .filter(message -> message.getMessage().getAuthor().map(user -> !user.isBot()).orElse(false))
