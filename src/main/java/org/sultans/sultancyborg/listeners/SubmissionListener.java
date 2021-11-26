91966cd2cf6746756be202b2c5ed1477461dcfd9
ShadesOfRay <r.y.wang00@gmail.com>
2021-11-26 11:18:51 -0500

    Added working submission listener test

--- src/main/java/org/sultans/sultancyborg/listeners/SubmissionListener.java
+++ src/main/java/org/sultans/sultancyborg/listeners/SubmissionListener.java
@@ -3,7 +3,12 @@ package org.sultans.sultancyborg.listeners;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.channel.MessageChannel;
 
-public class SubmissionListener implements ChannelListener{
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class SubmissionListener implements ChannelListener {
 
     @Override
     public String channel() {
@@ -13,11 +18,28 @@ public class SubmissionListener implements ChannelListener{
     @Override
     public void action(MessageCreateEvent event) {
         MessageChannel channel = event.getMessage().getChannel().block();
-        channel.createMessage("haha yes").subscribe();
+
+        event.getMessage().getAttachments().stream()
+                .map(attachment -> attachment.getData().url())
+                .forEach(url -> {
+                    try {
+                        BufferedInputStream inputStream = new BufferedInputStream(new URL(url).openStream());
+                        String filename = url.substring(url.lastIndexOf('/')+1);
+                        FileOutputStream fileOS = new FileOutputStream("data/test/" + filename);
+                        byte data[] = new byte[1024];
+                        int byteContent;
+                        while ((byteContent = inputStream.read(data, 0, 1024)) != -1) {
+                            fileOS.write(data, 0, byteContent);
+                        }
+                        channel.createMessage(String.format("Successfully downloaded `%s`", filename)).subscribe();
+                    } catch (IOException e) {
+                        System.out.println("Error downloading attachment");
+                    }
+                });
     }
 
     @Override
     public void log(MessageCreateEvent event) {
-
+    //TODO
     }
 }
