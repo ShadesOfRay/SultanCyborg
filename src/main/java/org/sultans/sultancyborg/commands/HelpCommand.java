8dbb8b8a7db44ec89df0402fddd24d1855fd0ef1
ShadesOfRay <r.y.wang00@gmail.com>
2022-08-23 21:37:34 -0400

    Hard coded the help section for the music commands

--- src/main/java/org/sultans/sultancyborg/commands/HelpCommand.java
+++ src/main/java/org/sultans/sultancyborg/commands/HelpCommand.java
@@ -29,6 +29,20 @@ public class HelpCommand implements Command{
             .flatMap(channel -> channel.createEmbed(spec -> {
                 spec.setTitle("SultanCyborg Commands");
                 SultanCyborgMain.commands.forEach(command -> spec.addField(STATIC.PREFIX + command.invoker(), command.info(), false));
+                // Hard coded for the other bot...
+                spec.addField("sult!join", "Has the bot join the VC", false);
+                spec.addField("sult!leave", "Has the bot leave the VC", false);
+                spec.addField("sult!mute", "Mute the bot", false);
+                spec.addField("sult!deafen", "Deafen the bot", false);
+                spec.addField("sult!play", "Have the bot play a song\n Must be in a channel first", false);
+                spec.addField("sult!pause", "Pause the currently playing song", false);
+                spec.addField("sult!resume", "Resume playing the currently paused song", false);
+                spec.addField("sult!skip", "skip the current song", false);
+                spec.addField("sult!quiet", "Quiet - Stop playing if bad sfx, use play after", false);
+                spec.addField("sult!stop", "stop playing and remove all songs from the queue", false);
+                spec.addField("sult!queue", "Show the current queue and what's playing right now", false);
+                spec.addField("sult!remove", "Remove a song from the queue", false);
+                spec.addField("sult!sfx", "Overlay another sound", false);
             }))
             .subscribe();
     }
